d755f6cae4d3e917edfb60815f1247d872ab05b6
mroatmeal <jfeind101@gmail.com>
2024-10-17 14:46:49 -0500

    end of exercise 1

--- exercise.py
+++ exercise.py
@@ -3,4 +3,5 @@ def product(a, b):
     """Return product of a and b.
         >>> product(2, 2) = 4 >>> product(2, -2) = -4
     """
-    
+    return a * b 
+print(product(2,2), ',', product(2,-2)) # 4 , -4
