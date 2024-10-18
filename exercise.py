3ee6ebdbe09c30d77698c529abb41e3015f06163
mroatmeal <jfeind101@gmail.com>
2024-10-18 13:13:46 -0500

    start of exercise 7

--- exercise.py
+++ exercise.py
@@ -106,20 +106,22 @@
 # print(reverse_string('sauce')) # ecuas
 
 # Exercise 6 Single Letter Count
-def single_letter_count(word, letter):
-    """How many times does letter appear in word (case-insensitively)?
-        >>> single_letter_count('Hello World', 'h')
-        1
-        >>> single_letter_count('Hello World', 'z')
-        0
-        >>> single_letter_count("Hello World", 'l')
-        3
-    """
-    # turing the variable input into a list after it has been lower cased
-    list_word = list(word.lower())
-    # return the number of times the variable 'letter' appears in the list 'list_word'
-    return list_word.count(letter)
+# def single_letter_count(word, letter):
+#     """How many times does letter appear in word (case-insensitively)?
+#         >>> single_letter_count('Hello World', 'h')
+#         1
+#         >>> single_letter_count('Hello World', 'z')
+#         0
+#         >>> single_letter_count("Hello World", 'l')
+#         3
+#     """
+#     # turing the variable input into a list after it has been lower cased
+#     list_word = list(word.lower())
+#     # return the number of times the variable 'letter' appears in the list 'list_word'
+#     return list_word.count(letter)
 
-print(single_letter_count('Hello World', 'h'))
-print(single_letter_count('Hello World', 'z'))
-print(single_letter_count('Hello World', 'l'))
+# print(single_letter_count('Hello World', 'h'))
+# print(single_letter_count('Hello World', 'z'))
+# print(single_letter_count('Hello World', 'l'))
+
+# Exercise 7 Multiple Letter Count
