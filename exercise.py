bdfadd2ee495e5687468fd1ee3bb30adad8ec3ba
mroatmeal <jfeind101@gmail.com>
2024-10-18 17:12:33 -0500

    end of exercise 8

--- exercise.py
+++ exercise.py
@@ -144,3 +144,62 @@
 # print(multiple_letter_count('yayyyyy'))
 # print(multiple_letter_count('Yaaay'))
 # print(multiple_letter_count('hello world'))
+
+# Exercise 8 List Manipulation
+def list_manipulation(lst, command, location, value=None):
+    """Mutate lst to add/remove from beginning or end.
+
+    - lst: list of values
+    - command: command, either "remove" or "add"
+    - location: location to remove/add, either "beginning" or "end"
+    - value: when adding, value to add
+
+    remove: remove item at beginning or end, and return item removed
+        >>> lst = [1, 2, 3]
+        >>> list_manipulation(lst, 'remove', 'end') 3
+        >>> list_manipulation(lst, 'remove', 'beginning') 1
+        >>> lst [2]
+
+    add: add item at beginning/end, and return list
+        >>> lst = [1, 2, 3]
+        >>> list_manipulation(lst, 'add', 'beginning', 20) [20, 1, 2, 3]
+        >>> list_manipulation(lst, 'add', 'end', 30) [20, 1, 2, 3, 30]
+        >>> lst [20, 1, 2, 3, 30]
+
+    Invalid commands or locations should return None:
+        >>> list_manipulation(lst, 'foo', 'end') is None True
+        >>> list_manipulation(lst, 'add', 'dunno') is None True
+    """
+    if value == None:
+        if command == 'remove' and location == 'end':
+            lst.pop()              
+         
+        if command == 'remove' and location == 'beginning':
+            lst.pop(0)
+
+    if value:
+        if command == 'add' and location == 'beginning':
+            lst.insert(0, value)
+        
+        if command == 'add' and location == 'end':
+            lst.append(value)
+
+    if 'add' not in command and 'remove' not in command:
+            return 'None'
+            # if location != 'end':
+            #     return 'None'
+
+    if 'end' not in location and 'beginning' not in location:
+         return 'None'   
+
+    
+    return lst
+        
+
+lst = [1,2,3]
+# print(list_manipulation(lst, 'remove', 'end')) # [1, 2]
+# print(list_manipulation(lst, 'remove', 'beginning')) # [2]
+print(list_manipulation(lst, 'add', 'beginning', 20)) # [20, 1, 2, 3]
+print(list_manipulation(lst, 'add', 'end', 30)) # [20, 1, 2, 3, 30]
+print(list_manipulation(lst, 'foo', 'end')) # None
+print(list_manipulation(lst, 'add', 'dunno')) # None
