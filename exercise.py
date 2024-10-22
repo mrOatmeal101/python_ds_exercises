c26e95b3a2da9420c34346d9f035b0009c867ec6
mroatmeal <jfeind101@gmail.com>
2024-10-22 14:45:13 -0500

    end of exercise 16

--- exercise.py
+++ exercise.py
@@ -404,3 +404,48 @@
 # print(intersection([1, 2, 3], [3, 4])) # [3]
 # print(intersection([1, 2, 3], [4, 5, 6])) # []
 # print(intersection([1, 2, 3, 6], [4, 5, 6, 1])) # [1, 6]
+
+# Exercise 16 Partition
+def partition(lst, fn):
+    """Partition lst by predicate. 
+     - lst: list of items
+     - fn: function that returns True or False 
+     Returns new list: [a, b], where `a` are items that passed fn test,
+     and `b` are items that failed fn test.
+        >>> def is_even(num):
+        ...     return num % 2 == 0    
+        >>> def is_string(el):
+        ...     return isinstance(el, str)     
+        >>> partition([1, 2, 3, 4], is_even)
+        [[2, 4], [1, 3]]    
+        >>> partition(["hi", None, 6, "bye"], is_string)
+        [['hi', 'bye'], [None, 6]]
+    """ 
+    return fn(lst)        
+
+def is_even(val):
+    even = []
+    odd = [] 
+
+    for items in val:
+        if items % 2 == 0:
+            even.append(items)
+        else:
+            odd.append(items)
+
+    return [even, odd]
+
+def is_string(arg):
+    strings = []
+    not_strings = []
+
+    for items in arg:
+        if isinstance(items, str):
+            strings.append(items)
+        else:
+            not_strings.append(items)
+
+    return [strings, not_strings]
+
+print(partition([1, 2, 3, 4], is_even)) # [[2, 4], [1, 3]]
+print(partition(["hi", None, 6, "bye"], is_string)) # [['hi', 'bye'], [None, 6]]
