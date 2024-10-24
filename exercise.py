edd6283ab45d541a92238861572ba425a1c3fc56
mroatmeal <jfeind101@gmail.com>
2024-10-24 13:59:34 -0500

    end of exercise 24

--- exercise.py
+++ exercise.py
@@ -686,5 +686,22 @@
 # print(list_check([[1], [2, 3]])) # True
 # print(list_check([[1], "nope"]))  # False
 
+# Exercise 24 remove_every_othe
+def remove_every_other(lst):
+    """Return a new list of other item.
+        >>> lst = [1, 2, 3, 4, 5]
+        >>> remove_every_other(lst)
+        [1, 3, 5]
+    This should return a list, not mutate the original:
+        >>> lst
+        [1, 2, 3, 4, 5]
+    """
+    every_other = []
 
-    
+    for i in range(0, len(lst),2):
+        every_other.append(lst[i])
+
+    return f'new list is {every_other}, original list is {lst}'
+
+lst = [1, 2, 3, 4, 5]
+print(remove_every_other(lst)) # new list is [1, 3, 5], original list is [1, 2, 3, 4, 5]    
