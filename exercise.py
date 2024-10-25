937c7236b74b31dfe0b3644e07f227a5b41afd11
mroatmeal <jfeind101@gmail.com>
2024-10-25 14:19:32 -0500

    end of exercise 31

--- exercise.py
+++ exercise.py
@@ -858,29 +858,62 @@
 # print(includes({"apple": "red", "berry": "blue", 'blackberry': 'purple', "peach": "yellow-orange"}, "yellow-orange")) # True
 
 # Exercise 30 repeat
-def repeat(phrase, num):
-    """Return phrase, repeated num times.
-        >>> repeat('*', 3)
-        '***'
-        >>> repeat('abc', 2)
-        'abcabc'
-        >>> repeat('abc', 0)
-        ''
-    Ignore illegal values of num and return None:
-        >>> repeat('abc', -1) is None
-        True
-        >>> repeat('abc', 'nope') is None
-        True
-    """
-    if isinstance(num, int) and num >= 0:
-        repeats = []
-        for item in phrase:
-            repeats.append(item)
-        return f"'{''.join(repeats) * num}'"
-    return f"('{phrase}','{num}') is {None} {True}"
+# def repeat(phrase, num):
+#     """Return phrase, repeated num times.
+#         >>> repeat('*', 3)
+#         '***'
+#         >>> repeat('abc', 2)
+#         'abcabc'
+#         >>> repeat('abc', 0)
+#         ''
+#     Ignore illegal values of num and return None:
+#         >>> repeat('abc', -1) is None
+#         True
+#         >>> repeat('abc', 'nope') is None
+#         True
+#     """
+#     if isinstance(num, int) and num >= 0:
+#         repeats = []
+#         for item in phrase:
+#             repeats.append(item)
+#         return f"'{''.join(repeats) * num}'"
+#     return f"('{phrase}','{num}') is {None} {True}"
 
-print(repeat('*', 3)) # '***'
-print(repeat('abc', 2)) # 'abcabc'
-print(repeat('abc', 0)) # ''
-print(repeat('abc', -1)) # ('abc','-1') is None True
-print(repeat('abc', 'nope')) # ('abc','nope') is None True
+# print(repeat('*', 3)) # '***'
+# print(repeat('abc', 2)) # 'abcabc'
+# print(repeat('abc', 0)) # ''
+# print(repeat('abc', -1)) # ('abc','-1') is None True
+# print(repeat('abc', 'nope')) # ('abc','nope') is None True
+
+# Exercise 31 truncate
+def truncate(phrase, n):
+    """Return truncated-at-n-chars version of phrase.
+    If the phrase is longer than, or the same size as, n make sure it ends with '...' and is no
+    longer than n.  
+        >>> truncate("Hello World", 6)
+        'Hel...'       
+        >>> truncate("Problem solving is the best!", 10)
+        'Problem...'
+        >>> truncate("Yo", 100)
+        'Yo'
+
+    The smallest legal value of n is 3; if less, return a message:
+        >>> truncate('Cool', 1)
+        'Truncation must be at least 3 characters.'
+        >>> truncate("Woah", 4)
+        'W...'
+        >>> truncate("Woah", 3)
+        '...'
+    """
+    if n < 3:
+        return 'Truncation must be at least 3 characters.'
+    if len(phrase) < n:
+        return phrase
+    return phrase[:n - 3] + "..."
+
+print(truncate("Hello World", 6)) # Hel...
+print(truncate("Problem solving is the best!", 10)) # Problem...
+print(truncate("Yo", 100)) # Yo
+print(truncate('Cool', 1)) # Truncation must be at least 3 characters.
+print(truncate("Woah", 4)) # W...
+print(truncate("Woah", 3)) # ...
